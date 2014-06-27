14e1ee1a30c3270269758ed72619a098a8f1051a
azardilis <azardilis@gmail.com>
2014-06-27 14:13:26 +0100

    same scale for all heatmaps

--- exp.py
+++ exp.py
@@ -2,7 +2,6 @@ import numpy as np
 from itertools import product
 import sim
 import matplotlib.pyplot as plt
-import operator
 
 def calc_dists(L, B, C, n_steps):
     # Given ranges for params calc variances, averages
@@ -22,27 +21,27 @@ def calc_dists(L, B, C, n_steps):
 
 def plot_vars(vars):
     n_points = 20
-    C = np.linspace(1.0, 3.0, n_points)
-    L = np.linspace(3.0, 5.0 ,n_points)
+    L = np.linspace(1.0, 2.0, n_points)
+    B = np.linspace(1.0, 2.0 ,n_points)
 
     vars_x1 = np.reshape(vars[:, 0], (n_points, n_points))
     vars_x2 = np.reshape(vars[:, 1], (n_points, n_points))
 
     plt.subplot(121)
-    plt.imshow(vars_x1, interpolation='none', aspect=20/20.)
+    plt.imshow(vars_x1, interpolation='none', aspect=20/20., vmin=0.5, vmax=1.8)
     plt.xlabel(r'C')
     plt.ylabel(r'$\lambda$')
-    plt.xticks(np.arange(0, n_points, 4), np.around(C[np.arange(0, n_points, 4)], 1))
+    plt.xticks(np.arange(0, n_points, 4), np.around(B[np.arange(0, n_points, 4)], 1))
     plt.yticks(np.arange(0, n_points, 4), np.around(L[np.arange(0, n_points, 4)], 1))
-    plt.title(r'CV $x_2$')
+    plt.title(r'CV $x_1$')
     cticks1 = np.around(np.linspace(np.min(vars_x1), np.max(vars_x1), 4), 1)
     plt.colorbar(shrink=.5, aspect=10., ticks=cticks1)
     
     plt.subplot(122)
-    plt.imshow(vars_x2, interpolation='none', aspect=20/20.)
+    plt.imshow(vars_x2, interpolation='none', aspect=20/20.,vmin=0.7, vmax=1.8)
     plt.xlabel(r'C')
     plt.ylabel(r'$\lambda$')
-    plt.xticks(np.arange(0, n_points, 4), np.around(C[np.arange(0, n_points, 4)], 1))
+    plt.xticks(np.arange(0, n_points, 4), np.around(B[np.arange(0, n_points, 4)], 1))
     plt.yticks(np.arange(0, n_points, 4), np.around(L[np.arange(0, n_points, 4)], 1))
     plt.title(r'CV $x_2$')
     cticks2 = np.around(np.linspace(np.min(vars_x2), np.max(vars_x2), 4), 1)
@@ -57,7 +56,8 @@ def plot_dev_hist(dists):
     devs = sim.get_devs(dists)
     plt.hist(devs, alpha=0.8)
     plt.title("Deviations of the CVs between species")
-    plt.xlabel(r"$|(\sigma_{x_1}^2/\langle x_1 \rangle^2)-(\sigma_{x_2}^2/\langle x_2 \rangle^2)|$")
+    #plt.xlabel(r"$(\sigma_{x_1}^2/\langle x_1 \rangle^2)-(\sigma_{x_2}^2/\langle x_2 \rangle^2)$")
+    plt.xlabel(r"$\langle x_1 \rangle - \langle x_2 \rangle$")
     plt.ylabel("Frequency")
     plt.show()
     
@@ -65,13 +65,14 @@ def plot_approx_vars(avars, vars):
     # plot of the exact variances against
     # the approximate variances calculate in terms
     # of avgs and Efficiencies
-    plt.plot(vars, avars, 'o', color='w')
-    plt.xlabel("Exact variance")
-    plt.ylabel("Approximate variance")
+    plt.plot(vars[:, 0], avars, 'wo', vars[:, 0], vars[:, 0])
+    plt.xlabel("Exact CV")
+    plt.ylabel("Approximate CV")
     plt.show()
-    plt.close()
-    plt.hist(np.abs(vars-avars), alpha=0.8)
-    plt.xlabel("|Exact var - approx var|")
+    
+def plot_approx_var_dev(avars, vars):
+    plt.hist(avars-vars[:, 0])
+    plt.xlabel("Approximate CV - Exact CV")
     plt.ylabel("Frequency")
     plt.show()
     
